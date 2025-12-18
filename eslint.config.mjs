624dfaca7c478e5b8160745d2090114c003ac513
felix11 <khushkothari06@gmail.com>
2025-12-18 15:23:12 +0400

    eslint fixes

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,23 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
-
-// Remove any leading/trailing whitespace from global keys
-const cleanedGlobals = Object.fromEntries(
-  Object.entries(globals.browser).map(([key, value]) => [key.trim(), value]),
-);
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
-  { languageOptions: { globals: cleanedGlobals } },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.node } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
   {
     rules: {
-      "@typescript-eslint/no-explicit-any": "off",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
       ],
-      "no-fallthrough": "off",
-      "no-constant-condition": "off",
-      "prefer-const": "warn",
+      'no-fallthrough': 'off',
+      'no-constant-condition': 'off',
+      'prefer-const': 'warn',
     },
   },
 ];
