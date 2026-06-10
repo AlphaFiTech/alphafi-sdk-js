c8e2ba8e0d0ff2cc89495e613dfe361935afe19f
Pankaj Jangid <pankaj.jangid@gmail.com>
2026-06-10 12:48:15 -0700

    docs(index): note README for usage examples

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@
  * AlphaFi SDK for JavaScript
  *
  * This SDK provides a comprehensive set of tools to interact with the AlphaFi platform.
+ * See the README for installation and usage examples.
  */
 
 // Export core modules
